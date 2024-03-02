330aa981ae75d84b135843ed7761dc4728f3ab05
KalweCaramalac <kalwe.dev@gmail.com>
2024-03-01 22:03:25 -0400

    create initial routes

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h2>React Cognito</h2>
+      <Outlet />
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
+import Home from './components/Home'
+import Dashboard from './components/Dashboard'
+import ErrorPage from './components/ErrorPage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/dashboard',
+        element: <Dashboard />,
+      },
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
